ff773733ac60c02b8d89a1f07defeb34b314a8c7
Diana Avila <45690382+dianafismed@users.noreply.github.com>
2024-01-20 17:10:14 -0300

    up

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,18 +1,34 @@
 const photo = document.getElementById('init-photo')
 const items = document.getElementById('init-items')
-
-
-
 const about = document.getElementById('about')
+const projects = document.getElementById('projetcs')
+
 const btnCv = document.getElementById('btn-cv')
-btnCv.addEventListener('click', ()=> {
+const btnProject = document.getElementById('btn-project')
+const btnVoltar = document.querySelector('.back')
+
+btnCv.addEventListener('click', ()=> open(about))
+btnVoltar.addEventListener('click', ()=> close())
+
+function open(open){
+  photo.classList.remove('left-reverse')
   photo.classList.add('left')
+  items.classList.remove('right-reverse')
   items.classList.add('right')
-  about.classList.remove('hidden')
-  about.classList.add('up')
-})
 
-/*
-  sectionCv.setAttribute("style","display:block")
-*/
+  open.classList.remove('up-reverse')
+  open.removeAttribute('hidden')
+  open.classList.add('up')
+}
 
+function close(){
+  console.log()
+  photo.classList.remove('left')
+  photo.classList.add('left-reverse')
+  items.classList.remove('right')
+  items.classList.add('right-reverse')
+
+  about.classList.remove('up')
+  about.setAttribute('hidden', 'true')
+  about.classList.add('up-reverse')
+}
